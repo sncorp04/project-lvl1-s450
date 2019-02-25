faeaa98e1ca35cd56f19541eeac5d5abdbf9f8bd
sncorp <a.a.malutin@gmail.com>
2019-02-25 17:31:29 +0300

    second commit

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
+import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
+console.log('');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
