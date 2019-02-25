4ce24ffafaaa254de980ef7a4da5fc3452da56d8
sncorp <a.a.malutin@gmail.com>
2019-02-25 21:03:51 +0300

    add Welcome Message

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+const welcomeMsg = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('');
+  const requestOfName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${requestOfName}!`);
+};
+export default welcomeMsg;
